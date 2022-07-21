e21c44c5ad2b454d4bd49b54e34c6372fdf74fc7
Volkov Alexander <volkov.525@gmail.com>
2022-07-21 18:36:46 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import { createApp } from 'vue'
+import { Quasar, Notify } from 'quasar'
+
+import '@quasar/extras/mdi-v6/mdi-v6.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+import '@/assets/styles/main.scss'
+import { createPinia } from 'pinia'
+import { router } from '@/router/router'
+
+import './style.css'
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+	plugins: {
+		Notify,
+	}, // import Quasar plugins and add here
+	config: {
+		notify: {
+			// type: 'warning',
+			position: 'bottom-right',
+			timeout: 3000,
+			icon: 'mdi-alert',
+			color: 'primary',
+			classes: 'notific',
+		},
+	},
+	supportTS: {
+		tsCheckerConfig: {
+			eslint: {
+				enabled: true,
+				files: './src/**/*.{ts,tsx,js,jsx,vue}',
+			},
+		},
+	},
+	/*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.use(createPinia())
+myApp.use(router)
+myApp.mount('#app')
