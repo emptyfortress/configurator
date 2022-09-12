dea1265fa3516ac3dd4430050dec3a5b339079a5
Volkov Alexander <volkov.525@gmail.com>
2022-09-12 09:47:19 +0300

    fix buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,26 +2,22 @@
 .cent
 	q-img(src="@/assets/img/bg.png" width="300px" height="300px").q-mt-xl
 	h6 Выберите тип настройки:
-	q-btn(color="accent" to="/start").q-mr-md
+	q-card-actions
+		q-btn(color="accent" to="/start")
 		q-icon(name="mdi-tools").q-mr-sm
 		|Первичная настройка
 	q-btn(color="accent" to="/refresh")
 		q-icon(name="mdi-reload").q-mr-sm
 		|Обновление настроек
 
-
-//- .share
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-//@import '@/assets/css/colors.scss';
-
 .cent {
 	width: 500px;
 	margin: 0 auto;
-	// background: #ccc;
 	img {
 		display: block;
 		margin: 0 auto;
