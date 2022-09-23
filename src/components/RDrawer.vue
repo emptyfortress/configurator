c8060a399d062db239082631b79de9b2460f67ba
Volkov Alexander <volkov.525@gmail.com>
2022-09-23 08:49:48 +0300

    new branch

--- src/components/RDrawer.vue
+++ src/components/RDrawer.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-q-drawer(v-model="props.show" side="right" bordered ).right
+q-drawer(v-model="props.show" side="right" bordered :class="calcClass").right
 	.q-mb-lg Console >
 	#output
 
@@ -7,8 +7,20 @@ q-drawer(v-model="props.show" side="right" bordered ).right
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useStore } from '@/stores/store'
+
+const mystore = useStore()
+
 const props = defineProps({
 	show: Boolean,
+})
+
+const calcClass = computed(() => {
+	if (mystore.os === 'linux') {
+		return 'lin'
+	}
+	return 'win'
 })
 </script>
 
--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,62 +1,88 @@
 <template lang="pug">
-q-page(padding :class="calcClass")
-	p Заполните формы или оставьте значения по умолчанию.
+q-page(padding)
+	p Заполните форму или оставьте значения по умолчанию.
 	.grid
-		.service(v-for="(item, index) in mystore.items" :key="item.id")
+		.service(v-for="item in mystore.items" :key="item.id")
 			.zag
 				|{{ item.label}}
-				transition(name="slide-right" )
-					q-btn(dense round flat icon="mdi-autorenew"
-						@click="reset(item, index)"
-						v-if="showRefresh(index)")
-						q-tooltip Вернуть умолчания
 			.form
 				.label Порт:
 				q-input(v-model="item.port" type="number" dense color="accent").port
 			br
-			q-expansion-item(dense
-				label="Дополнительные настройки")
+			q-expansion-item(dense label="Дополнительно")
 				q-card-section
-					q-input(v-model="item.log" type="text" label="Хранилище логов" color="accent" clearable)
+					q-input(v-if="mystore.os === 'windows'" v-model="item.log" type="text" label="Хранилище логов" color="accent" clearable)
+					q-input(v-else v-model="item.loglinux" type="text" label="Хранилище логов" color="accent" clearable)
+
+	.service.item.pad
+		.zag База данных
+		.form
+			.label.wide Тип сервера:
+			q-select(v-model="mystore.serverType" :options="servertypes"  dense color="accent").port
+			.label.wide Название базы:
+			q-input(v-model="mystore.databaseName" type="text" dense color="accent").port
+			.label Аутентификация:
+			.q-gutter-lg
+				q-radio(color="accent" v-model="mystore.databaseAuth" val="Windows" label="Windows")
+				q-radio(color="accent" v-model="mystore.databaseAuth" val="SQL Server" label="SQL Server")
+			.label Логин:
+			q-input(v-model="mystore.login" dense color="accent").port
+			.label Пароль:
+			q-input(v-model="mystore.password" :type="calcType" dense color="accent").port
+				template(v-slot:append)
+					q-btn(dense flat round @click="pass = !pass")
+						q-icon(v-if="pass === true" name="mdi-eye-off")
+						q-icon(v-else name="mdi-eye")
+			.label
+			.row.justify-between.items-end
+				q-btn(flat color="accent" label="Проверка соединения" size="md" :loading="loading" :disable="checkPass" @click="simulateProgress").q-mt-lg
+				q-icon(v-if="connection && validate" name="mdi-check-network" size="32px" color="positive")
+				q-icon(v-if="connection && !validate" name="mdi-alert" size="32px" color="negative").cursor-pointer
+					q-menu(padding)
+						q-card-section Сервер не отвечает :(
 
 	q-card-actions(align="center").service.item
 		transition(name="slide-botton")
-			q-btn(color="accent" flat label="Сбросить изменения" @click="resetAll" :disable="!showApply")
+			q-btn(color="accent" flat label="Сбросить изменения" @click="resetAll" :disable="!showApply" icon="mdi-restore")
 				q-tooltip Вернуть значения по умолчанию
 		q-btn(color="accent" unelevated label="Применить")
 
 </template>
 
 <script setup lang="ts">
-import { ref, computed, reactive } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from '@/stores/store'
 import { useQuasar } from 'quasar'
-import type { Ref } from 'vue'
 import { servertypes } from '@/stores/data'
 
 const mystore = useStore()
 const $q = useQuasar()
 
-const user = ref([false, false, false])
-const pass: Ref<Boolean[]> = ref([false, false, false])
-const loading = ref([false, false, false])
-const connection = ref([false, false, false])
-const authentifications = ref(['SQL Server', 'Windows'])
+const pass = ref(true)
+const loading = ref(false)
+const connection = ref(false)
 
-const calcClass = computed(() => {
-	if (mystore.os === 'linux') {
-		return 'lin'
+const checkPass = computed(() => {
+	if (mystore.login.length > 2 && mystore.password.length > 2) {
+		return false
 	}
-	return 'win'
+	return true
 })
 
-const simulateProgress = (e: number) => {
-	loading.value[e] = true
+const validate = computed(() => {
+	if (mystore.login === 'kmg01' && mystore.password === 'kmg001') {
+		return true
+	}
+	return false
+})
+
+const simulateProgress = () => {
+	loading.value = true
 	const output = document.querySelector('#output')
 	setTimeout(() => {
-		loading.value[e] = false
-		connection.value[e] = true
-		if (e < 2) {
+		loading.value = false
+		connection.value = true
+		if (validate.value === true) {
 			$q.notify({
 				icon: 'mdi-check-bold',
 				message: 'Сервер в порядке',
@@ -72,27 +98,32 @@ const simulateProgress = (e: number) => {
 		}
 	}, 2000)
 }
-const calcType = (e: number) => {
-	if (pass.value[e] === false) {
+const calcType = computed(() => {
+	if (pass.value === true) {
 		return 'password'
 	} else return 'text'
-}
+})
 
-const reset = (e: any, i: number) => {
-	Object.assign(e, mystore.defaultItems[i])
-}
+// const reset = (e: any, i: number) => {
+// 	Object.assign(e, mystore.defaultItems[i])
+// }
 
 const resetAll = () => {
-	mystore.items.forEach((_, index) => {
-		Object.assign(mystore.items[index], mystore.defaultItems[index])
-	})
-	mystore.toggleRightDr()
-	connection.value = [false, false, false]
+	// mystore.items.forEach((_, index) => {
+	// 	Object.assign(mystore.items[index], mystore.defaultItems[index])
+	// })
+	// mystore.toggleRightDr()
+	connection.value = false
 }
 
 const showRefresh = (e: number) => {
 	if (JSON.stringify(mystore.items[e]) === JSON.stringify(mystore.defaultItems[e])) {
-		return false
+		return (
+			false ||
+			mystore.databaseAuth === 'Windows' ||
+			mystore.serverType === 'Microsoft SQL Server' ||
+			mystore.databaseName === 'DV\\SQL-2022'
+		)
 	} else return true
 }
 const showApply = computed(() => {
@@ -111,7 +142,7 @@ const showApply = computed(() => {
 	align-self: start;
 }
 .grid {
-	margin-top: 2rem;
+	margin-top: 1rem;
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
 	gap: 2rem;
@@ -119,6 +150,7 @@ const showApply = computed(() => {
 }
 .zag {
 	font-size: 1.1rem;
+	font-weight: 600;
 	margin-bottom: 1rem;
 	height: 32px;
 	display: flex;
@@ -127,12 +159,15 @@ const showApply = computed(() => {
 }
 .label {
 	font-size: 0.9rem;
-	width: 100px;
+	width: 80px;
+	&.wide {
+		width: 150px;
+	}
 }
 .form {
 	display: grid;
 	grid-template-columns: auto 1fr;
-	gap: 1rem;
+	gap: 0.5rem;
 	> div {
 		align-self: center;
 		white-space: no-wrap;
@@ -150,26 +185,26 @@ const showApply = computed(() => {
 	margin-top: 1rem;
 }
 .item {
-	width: 800px;
+	width: 600px;
 	margin: 0 auto;
 	margin-top: 2rem;
 	.q-card {
 		background: transparent;
 	}
-}
-.log {
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	gap: 1rem;
-	.q-input {
-		width: 60%;
+	&.pad {
+		padding: 1rem 2rem;
 	}
 }
+// .log {
+// 	display: flex;
+// 	justify-content: flex-start;
+// 	align-items: center;
+// 	gap: 1rem;
+// 	.q-input {
+// 		width: 60%;
+// 	}
+// }
 p {
 	font-size: 1rem;
-}
-.win {
-	background: img('@/assets/img/linux.svg');
 }
 </style>
