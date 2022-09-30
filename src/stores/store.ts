922ffe69f52d5a62a1d02eadf49568b97e0bb759
Volkov Alexander <volkov.525@gmail.com>
2022-09-30 13:54:44 +0300

    agree checkbox

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -6,7 +6,7 @@ export const useStore = defineStore({
 		leftDrawer: true,
 		rightDrawer: false,
 		mini: false,
-		agree: true,
+		agree: false,
 		os: 'windows',
 		databaseAuth: 'Windows',
 		connection: false,
