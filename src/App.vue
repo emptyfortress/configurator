ea62b666824e491c983fd8f52dc0ed59db53871d
Volkov Alexander <volkov.525@gmail.com>
2022-09-30 14:02:10 +0300

    toolbar fix

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ q-layout(view="LHh lpR fFf")
 		q-toolbar.text-black
 			q-toolbar-title.q-ml-md
 				|{{route.meta.title}}
-				span(v-if="mystore.os === 'windows'").q-ml-md (windows)
-				span(v-else).q-ml-md (linux)
+				span(v-if="mystore.os === 'windows' && route.fullPath !== '/'").q-ml-md (windows)
+				span(v-if="mystore.os === 'linux' && route.fullPath !== '/'").q-ml-md (linux)
 
 			q-btn(flat round @click="en = !en")
 				img(v-if="en" src="@/assets/img/icons/us.svg" width="24")
